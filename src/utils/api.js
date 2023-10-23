b213239518726b6dfe8976025e4c95442ce4905c
yogendra09 <yogendrayadav168@gmail.com>
2023-10-23 14:44:39 +0530

    youtube_clone

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":'1c9fc20a5fmsh07af003f0f0a01ep1fdd90jsnf4b4d56c2789',
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
